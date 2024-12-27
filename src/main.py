74fb8b338e06256d4be0c8b9de46ea6ab7e610dd
Pavel Koshkin <koshkin.pasha@gmail.com>
2024-12-27 10:59:53 +0100

    minor changes: .env.sample, main.py

--- src/main.py
+++ src/main.py
@@ -1,15 +1,22 @@
 from fastapi import FastAPI
 from dotenv import load_dotenv
 import os
-
-app = FastAPI()
+import requests
 
 load_dotenv()
+
+SERVER_IP = os.environ["SERVER_IP"]
+SERVER_PORT = os.environ["SERVER_PORT"]
+
+SERVER = f"{SERVER_IP}:{SERVER_PORT}"
+
+app = FastAPI()
 
 
 @app.get("/")
 def read_root():
-    return {"message": "Hello there"}
+    response = requests.get(SERVER)
+    return {"response":response.text}
 
 @app.get("/models")
 def list_models():
